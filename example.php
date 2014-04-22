6298e7d8a894d57c64b547f348a16f74912d92bd
Daniel Marquard <daniel@marquardholdings.com>
2014-04-22 12:18:10 -0400

    Added recovery email argument.

--- example.php
+++ example.php
@@ -2,43 +2,47 @@
 
 include('GoogleVoice.php');
 
-// NOTE: Full email address required.
-$gv = new GoogleVoice('your.email@domain.com', 'password');
+// Provide your credentials. NOTE: Full email address required.
+$accountEmail = 'your.email@domain.com';
+$password = 'password';
+$recoveryEmail = '';   // Optional: Google may request this.
+
+$gv = new GoogleVoice($accountEmail, $password, $recoveryEmail);
 
 // Call a phone from one of your forwarding phones.
 $gv->callNumber('9995551212', '5558675309', 'mobile');
 
-// Cancel in-progress call placed using callNumber.
+// Cancel an in-progress call placed using callNumber.
 $gv->cancelCall('9995551212', '5558675309', 'mobile');
 
 // Send an SMS to a phone number.
 $gv->sendSMS('9995551212', 'Sending a message!');
 
-// Get all unread SMSs from your Google Voice Inbox.
+// Get all unread SMSs from your Google Voice inbox.
 $sms = $gv->getUnreadSMS();
 $msgIDs = array();
 foreach($sms as $s) {
 	echo 'Message from: '.$s->phoneNumber.' on '.$s->displayStartDateTime.': '.$s->messageText."<br><br>\n";
 	if(!in_array($s->id, $msgIDs)) {
-		// Mark the message as read in your Google Voice Inbox.
+		// Mark the message as read.
 		$gv->markMessageRead($s->id);
 		$msgIDs[] = $s->id;
 	}
 }
 
-// Get all unread messages from your Google Voice Voicemail.
+// Get all unread messages from your Google Voice voicemail.
 $voice_mails = $gv->getUnreadVoicemail();
 $msgIDs = array();
 foreach($voice_mails as $v) {
 	echo 'Message from: '.$v->phoneNumber.' on '.$v->displayStartDateTime.': '.$v->messageText."<br><br>\n";
 	if(!in_array($v->id, $msgIDs)) {
-		// Mark the message as read in your Google Voice Voicemail.
+		// Mark the message as read.
 		$gv->markMessageRead($v->id);
 		$msgIDs[] = $v->id;
 	}
 }
 
-// Download all unread Google Voice voicemails as indididual MP3 files.
+// Download all unread Google Voice voicemails as individual MP3 files.
 $voice_mails = $gv->getUnreadVoicemail();
 $msgIDs = array();
 foreach($voice_mails as $v) {
@@ -53,7 +57,7 @@ foreach($voice_mails as $v) {
 	fwrite($fh, $mp3);
 	fclose($fh);
 	if(!in_array($v->id, $msgIDs)) {
-		// Mark the message as read in your Google Voice Voicemail.
+		// Mark the message as read.
 		$gv->markMessageRead($v->id);
 		$msgIDs[] = $v->id;
 	}
